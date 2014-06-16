6e1ae365e2a4dac418d38e84f0830dd3824de948
Luis Lobo Borobia <luislobo@gmail.com>
2014-06-16 13:41:33 -0300

    Updated to the new Sails.js structure (up to RC8)
    Updated to the new Sails.js structure (up to RC8)

--- config/connections.js
+++ config/connections.js
@@ -1,15 +1,22 @@
 /**
  * Connections
- * 
- * The `connections` configuration object lets you create different global "saved settings"
- * that you can mix and match in your models.  The `default` option indicates which 
- * "saved setting" should be used if a model doesn't have a connection specified.
+ * (sails.config.connections)
  *
- * Note: If you're using version control, you should put your passwords/api keys 
- * in `config/local.js`, not here, in case you inadvertently push them up to your repository.
+ * `Connections` are like "saved settings" for your adapters.  What's the difference between
+ * a connection and an adapter, you might ask?  An adapter (e.g. `sails-mysql`) is generic--
+ * it needs some additional information to work (e.g. your database host, password, user, etc.)
+ * A `connection` is that additional information.
+ *
+ * Each model must have a `connection` property (a string) which is references the name of one
+ * of these connections.  If it doesn't, the default `connection` configured in `config/models.js`
+ * will be applied.  Of course, a connection can (and usually is) shared by multiple models.
+ * .
+ * Note: If you're using version control, you should put your passwords/api keys
+ * in `config/local.js`, environment variables, or use another strategy.
+ * (this is to prevent you inadvertently sensitive credentials up to your repository.)
  *
  * For more information on configuration, check out:
- * http://sailsjs.org/#documentation
+ * http://links.sailsjs.org/docs/config/connections
  */
 
 module.exports.connections = {
@@ -29,10 +36,10 @@ module.exports.connections = {
   // npm install sails-mysql
   //
   someMysqlServer: {
-    adapter : 'sails-mysql',
-    host    : 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user    : 'YOUR_MYSQL_USER',
-    password: 'YOUR_MYSQL_PASSWORD', 
+    adapter: 'sails-mysql',
+    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
+    user: 'YOUR_MYSQL_USER',
+    password: 'YOUR_MYSQL_PASSWORD',
     database: 'YOUR_MYSQL_DB'
   },
 
@@ -43,26 +50,26 @@ module.exports.connections = {
   // npm install sails-mongo
   //
   someMongodbServer: {
-    adapter   : 'sails-mongo',
-    host      : 'localhost',
-    port      : 27017,
-    user      : 'username',
-    password  : 'password',
-    database  : 'your mongo db name here'
+    adapter: 'sails-mongo',
+    host: 'localhost',
+    port: 27017,
+    // user: 'username',
+    // password: 'password',
+    // database: 'your_mongo_db_name_here'
   },
 
-  // PostgreSQL is another officially supported relational database. 
+  // PostgreSQL is another officially supported relational database.
   // http://en.wikipedia.org/wiki/PostgreSQL
   //
   // Run:
   // npm install sails-postgresql
   //
   somePostgresqlServer: {
-    adapter   : 'sails-postgresql',
-    host      : 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user      : 'YOUR_POSTGRES_USER',
-    password  : 'YOUR_POSTGRES_PASSWORD', 
-    database  : 'YOUR_POSTGRES_DB'
+    adapter: 'sails-postgresql',
+    host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
+    user: 'YOUR_POSTGRES_USER',
+    password: 'YOUR_POSTGRES_PASSWORD',
+    database: 'YOUR_POSTGRES_DB'
   }
 
 
@@ -70,7 +77,3 @@ module.exports.connections = {
   // https://github.com/balderdashy/sails
 
 };
-
-
-
-
--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,6 @@
 /**
- * Routes
+ * Route Mappings
+ * (sails.config.routes)
  *
  * Your routes map URLs to views and controllers.
  * 
@@ -16,7 +17,7 @@
  * CoffeeScript for the front-end.
  *
  * For more information on routes, check out:
- * http://sailsjs.org/#documentation
+ * http://links.sailsjs.org/docs/config/routes
  */
 
 module.exports.routes = {
@@ -35,8 +36,8 @@ module.exports.routes = {
   // Custom routes here...
 
 
-  // If a request to a URL doesn't match any of the custom routes above, it is matched 
-  // against Sails route blueprints.  See `config/blueprints.js` for configuration options
-  // and examples.
+  // If a request to a URL doesn't match any of the custom routes above,
+  // it is matched against Sails route blueprints.  See `config/blueprints.js`
+  // for configuration options and examples.
 
 };
